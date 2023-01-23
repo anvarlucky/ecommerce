f8f62a2b30b4524794a6fa15472115593450da38
anvar086 <laraveldeveloper086@gmail.com>
2023-01-23 17:23:09 +0500

    vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
-//import './index.css';
+import store from './store'
+import router from './router'
+import './index.css';
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
