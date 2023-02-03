4590ee1f98a067c9937189a6b53379b20a62aa5c
anvar086 <laraveldeveloper086@gmail.com>
2023-02-03 17:43:00 +0500

    adasda

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\GoodsController;
+use App\Http\Controllers\api\v1\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [\App\Http\Controllers\api\v1\AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('goods',[GoodsController::class,'index']);
+    Route::get('good/{id}',[GoodsController::class,'show']);
+    Route::post('good',[GoodsController::class,'store']);
+    Route::get('types',[TypeController::class,'index']);
+    Route::post('type',[TypeController::class,'store']);
 });
 
-Route::post('login', [\App\Http\Controllers\api\v1\AuthController::class, 'login']);
+
